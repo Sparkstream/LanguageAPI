13f9c655b1f220d43e34d10c5dc3830f2ef91bb2
Phillip Quach <p.quach97@hotmail.com>
2018-11-19 15:08:48 +1300

    Added initial project files

---
+++ LanguageAPI/Model/LanguageItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageAPI.Model
+{
+    public class LanguageItem
+    {
+        public int Id { get; set; }
+        public string userId { get; set; }
+        public string sourceLanguage { get; set; }
+        public string targetLanguage { get; set; }
+        public string sourceWord { get; set; }
+        public string targetWord { get; set; }
+
+    }
+}
